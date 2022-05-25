f264ad36b0ba96d1b8c2ae71d6fb3310ad0716ce
BrandooDi <nikita1231w@gmail.com>
2022-05-25 15:49:39 +0300

    6

--- 2/main.cpp
+++ 2/main.cpp
@@ -29,3 +29,12 @@ for (i = 0; i < n; i++) sumf += F[i];
     if (suma / n > sumg / n)
     {
         if (suma / n > sumf / n)
+cout << "Наилучший предмет: Алгебра";
+        else
+            cout << "Наилучший предмет:Физика";
+    }
+    else
+        if (suma / n < sumg / n)
+        {
+            if (sumg / n < sumf / n)
+                cout << "Наилучший предмет:Физика";
