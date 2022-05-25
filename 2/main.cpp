07121b4c32195278c02f5613afd251d0e8475401
BrandooDi <nikita1231w@gmail.com>
2022-05-25 15:27:19 +0300

    3

--- 2/main.cpp
+++ 2/main.cpp
@@ -10,3 +10,9 @@ int main()
     A = (int*)malloc(n * sizeof(int));
     G= (int*)malloc(n * sizeof(int));
     F = (int*)malloc(n * sizeof(int));
+  int i, suma = 0, sumg = 0, sumf = 0;
+    cout << "Введите оценки по алгебре:\n";
+    for (i = 0; i < n; i++)
+        cin >> A[i];
+    for (i = 0; i < n; i++) suma += A[i];
+    cout << "Средняя оценка по алгебре:" << suma / n;
