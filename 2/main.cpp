12de6fb071b69d6be8db7c2f9badf695f89744d7
BrandooDi <nikita1231w@gmail.com>
2022-05-25 15:26:33 +0300

    2

--- 2/main.cpp
+++ 2/main.cpp
@@ -4,3 +4,9 @@ int main()
 {
     setlocale(LC_ALL, "Russian");
     const int n = 10;
+    int *A;
+    int *G;
+    int *F;
+    A = (int*)malloc(n * sizeof(int));
+    G= (int*)malloc(n * sizeof(int));
+    F = (int*)malloc(n * sizeof(int));
